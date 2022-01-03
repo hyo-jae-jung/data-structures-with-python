4ed4ece26b56147f91978b320a27d47f8d242c48
hyojaejung <hyojaejung@kakao.com>
2022-01-04 04:41:51 +0900

    chapter4, LinkedList done.

--- DoubleLinkedList/main.py
+++ DoubleLinkedList/main.py
@@ -1,9 +1,10 @@
 from DoubleLinkedList import DoubleLinkedList, show_list
 
 a = DoubleLinkedList()
-a.add_first('b')
-a.add_first('a')
-a.add_last('d')
-a.insert_after('c',a.search_forward('b'))
+a.add_first(2)
+a.add_first(1)
+a.add_first(3)
+# a.add_last('d')
+# a.insert_after('c',a.search_forward('b'))
 show_list(a)
 
